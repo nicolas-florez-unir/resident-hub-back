07153e16f466e763a364c6cc4f2f0fdee640103c
nflorez <nflorez@ubits.co>
2025-04-24 19:43:51 -0500

    feat: second version

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js', '.*.test.ts'],
+  ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
@@ -29,4 +29,10 @@ module.exports = {
       },
     ],
   },
+  overrides: [{
+    files: ['**/*.spec.ts', '**/*.test.ts'],
+    rules: {
+      'max-len': 'off'
+    }
+  }]
 };
